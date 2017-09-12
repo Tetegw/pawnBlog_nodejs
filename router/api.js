8f8d0920407db0ba9b5ae708d4cd29c46c90a5f4
Tetegw <1151832942@qq.com>
2017-09-12 23:23:29 +0800

    [Added] search done

--- router/api.js
+++ router/api.js
@@ -89,6 +89,45 @@ module.exports = function () {
         }
     });
 
+    // 获取搜索关键字文章列表
+    router.use('/search', (req, res, next) => {
+        let userId = req.query.userId || -1
+        let keword = req.query.searchKeyword
+        if (userId >= 0) {
+            // 通过用户ID获取文章列表
+            userId = userId - userIdPerfix
+            db.query("SELECT * FROM article_list WHERE userId=" + userId + " AND `mainTitle` LIKE '%" + keword + "%' OR `tags` LIKE '%" + keword + "%' OR `column` LIKE '%" + keword + "%' order by id desc", (err, data) => {
+                if (err) {
+                    res.status(500).send('数据库访问错误' + err)
+                } else {
+                    data.forEach(function (item) {
+                        var tags = item.tags.split('，');
+                        item.tags = tags;
+                        item.ID = prefix + item.ID;
+                        delete item.userId
+                    }, this);
+                    var rs = { list: data };
+                    res.send(rs)
+                }
+            })
+        } else {
+            db.query("SELECT * FROM article_list WHERE userId=1 AND `mainTitle` LIKE '%" + keword + "%' OR `tags` LIKE '%" + keword + "%' OR `column` LIKE '%" + keword + "%' order by id desc", (err, data) => {
+                if (err) {
+                    res.status(500).send('数据库访问错误' + err)
+                } else {
+                    data.forEach(function (item) {
+                        var tags = item.tags.split('，');
+                        item.tags = tags;
+                        item.ID = prefix + item.ID;
+                        delete item.userId
+                    }, this);
+                    var rs = { list: data };
+                    res.send(rs)
+                }
+            })
+        }
+    });
+
     // 获取栏目
     router.use('/categories', (req, res, next) => {
         let userId = req.query.userId || -1
